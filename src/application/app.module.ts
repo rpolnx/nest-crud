2f3b216165407abdae1c0e8b56a18b48815699ac
Rodrigo Pereira <rodrigorpogo@gmail.com>
2020-11-18 02:32:48 -0300

    Configuring global module to receive connections

--- src/application/app.module.ts
+++ src/application/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from './controller/app.controller'
 import { AppService } from './service/app.service'
-
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: async (configService: ConfigService) => ({
+                type: 'postgres',
+                host: configService.get('DB_HOST', 'localhost'),
+                port: configService.get<number>('DB_PORT', 5432),
+                username: configService.get('DB_USER', 'admin'),
+                password: configService.get('DB_PASS', 'pass'),
+                database: configService.get('DB_NAME', 'nest-crud'),
+                entities: [__dirname + '/**/*.entity{.ts,.js}'],
+            }),
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
