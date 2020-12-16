f142407c4c2b87ffe62b65512277806a911c6b0c
Rodrigo Pereira <rodrigorpogo@gmail.com>
2020-12-16 00:22:24 -0300

    Adding helmet and morgan

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core'
+import * as helmet from 'helmet'
+import * as morgan from 'morgan'
 import { AppModule } from './application/app.module'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
+    app.use(morgan('short'))
+    app.use(helmet())
     const port = process.env.PORT || 3000
     await app.listen(port)
     console.log(`Listen on PORT: ${port}`)
